3aebb37832f47fc1bfd3e6cace7813be4dd09aa2
muazsazelim <muazsazelim019@gmail.com>
2024-12-17 11:31:51 +0000

    rps game - without exception

--- script.js
+++ script.js
@@ -1,1 +1,73 @@
-console.log("Hello World")
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+
+    if (humanChoice === computerChoice) {
+        console.log("Draw")
+    }
+    else if (humanChoice === "rock" && computerChoice === "paper") {
+        console.log("Computer Wins")
+        computerScore++;
+    }
+    else if (humanChoice === "rock" && computerChoice === "scissor") {
+        console.log("Human Wins")
+        humanScore++;
+    }
+    else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log("Human Wins")
+        humanScore++;
+    }
+    else if (humanChoice === "paper" && computerChoice === "scissor") {
+        console.log("Computer Wins")
+        computerScore++;
+    }
+    else if (humanChoice === "scissor" && computerChoice === "paper") {
+        console.log("Human Wins")
+        humanScore++;
+    }
+    else if (humanChoice === "scissor" && computerChoice === "rock") {
+        console.log("Computer Wins")
+        computerScore++;
+    }
+
+
+}
+
+function getComputerChoice() {
+    let randomInt = Math.random()
+
+    if (randomInt <= 0.33) {
+        return "rock";
+    }
+    else if (randomInt <= 0.66) {
+        return "paper";
+    }
+    else {
+        return "scissor";
+    }
+}
+
+function getHumanChoice() {
+    return prompt("Human choice: ")
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+
+function playGame() {
+    for (i=0; i<5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+    }
+
+    console.log("Human:" + humanScore + ", Computer: " + computerScore)
+
+}
+
+playGame()
+
